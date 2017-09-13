099ccde6dbb29681c27f1033ae23eeb4bde66766
haitao.lan@longsys.com <haitao.lan@longsys.com>
2017-09-13 17:27:08 +0800

    新增函数

---
+++ task/函数.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# zzz.py
+# @Author : Bryan.Lan (18645555731@163.com)
+# @Link   : https://github.com/Bryan130
+# @Date   : 2017/9/4 下午2:38:07
+
+
+def outer():
+    count = 10
+
+    def inter():
+        print(count)
+        # count = 5
+        # print(count)
+    inter()
+    print(count)
+
+outer()
---
+++ task/斐波那契数列.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 -*-
+
+""" 
+@version: v1.0 
+@author: Bryan
+@license: Apache Licence  
+@contact: 244896035@qq.com 
+@site: http://http://blog.csdn.net/weixin_36650524 
+@software: PyCharm 
+@file: 斐波那契数列.py 
+@time: 2017/9/13 16:03 
+"""
+
+
+def fibo(n):
+    pass
+
+
+if __name__ == "__main__":
+    pass  
